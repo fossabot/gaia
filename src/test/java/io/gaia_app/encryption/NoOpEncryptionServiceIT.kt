748092c1f0f7ac154f7f0acadfd4a08c587b56ec
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-07-28 22:46:11 +0200

    :sparkles: : add NoOpEncryptionService
    This way, we can always rely on a encryption service, even if it does
    nothing at all.
    It will allow removal of null checks un CredentialsService.

---
+++ src/test/java/io/gaia_app/encryption/NoOpEncryptionServiceIT.kt
@@ -0,0 +1,19 @@
+package io.gaia_app.encryption
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest(classes = [NoOpEncryptionService::class])
+//internal class NoOpEncryptionServiceIT {
+//
+//    @Autowired
+//    lateinit var encryptionService: EncryptionService
+//
+//    @Test
+//    fun contextLoads(){
+//        assertThat(encryptionService).isNotNull()
+//    }
+//
+//}
